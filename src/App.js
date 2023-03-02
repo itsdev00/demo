5ba5a3a34c14b4f6f8d0a2a54acd9bfd9765d5ac
itsdev001 <devankmishra7050@gmail.com>
2023-03-02 19:02:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [limit, setLimit] = useState(5);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleLimitChange = (event) => {
+    const newLimit = parseInt(event.target.value);
+    if (newLimit < 5 || newLimit > 10) {
+      alert('Limit must be between 5 and 10');
+      return;
+    }
+    setLimit(newLimit);
+    setSearchResults([]);
+    setPage(1);
+    setTotalPages(1);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const options = {
+      method: 'GET',
+      url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
+      params: { countryIds: 'IN', namePrefix: searchTerm, limit: limit.toString(), offset: ((page - 1) * limit).toString() },
+      headers: {
+        'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+        'x-rapidapi-key': '4ac5e3352fmshe6ac515ca3b8ccap1f0045jsnf0a504a87bbe'
+      }
+    };
+    axios
+      .request(options)
+      .then(function (response) {
+        setSearchResults(response.data.data);
+        setTotalPages(Math.ceil(response.data.metadata.totalCount / limit));
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
+    return pageNumbers;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" value={searchTerm} onChange={handleChange} />
+          
+        </label>
+        <label>
+          Limit:
+          <input type="number" value={limit} min="5" max="10" onChange={handleLimitChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {searchResults.length === 0 ? (
+        <p>No results found.</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Place</th>
+              <th>Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            {searchResults.map((result, index) => (
+              <tr key={result.id}>
+                <td>{(page - 1) * limit + index + 1}</td>
+                <td>{result.city}</td>
+                <td>{result.country}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {totalPages > 1 && (
+        <div>
+          {getPageNumbers().map((pageNumber) => (
+            <button key={pageNumber} onClick={() => handlePageChange(pageNumber)}>
+              {pageNumber}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default SearchBar
